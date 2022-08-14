fafe6c2bec0f3181121164219b4b007ddb6e1cff
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2022-08-14 11:37:27 +0800

    Add files via upload

---
+++ Python Exercise 4.2.py
@@ -0,0 +1,5 @@
+speakers = {"Sir Rafael": 54, "Ms. Joan": 33, "Ms. Dana": 67}
+
+names = speakers.keys()
+
+print(names)
---
+++ Python Exercise 4.3.py
@@ -0,0 +1,3 @@
+tryout_results = {"Carl": "passed", "Quentin": "failed", "John Y.": "passed", "Peter": "failed", "Max T.": "passed", "Joseph": "passed", "Jone": "failed", "Jorge": "failed", "George": "passed", "Ben": "passed", "Jerome": "passed", "Rick": "failed", "Max G.": "failed", "John P.": "failed", "Vince": "passed"}
+
+print(tryout_results.get("Jorge"))
