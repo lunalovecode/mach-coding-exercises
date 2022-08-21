ccc1e8a7eb1cbf0bd01b941c42c734c6387a3a43
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2022-08-21 11:04:00 +0800

    Add files via upload

---
+++ Python Exercise 8.py
@@ -0,0 +1,3 @@
+from functions import divide_with_remainder, raise_to_power
+print(divide_with_remainder(10, 3))
+print(raise_to_power(4, 2))
---
+++ functions.py
@@ -0,0 +1,17 @@
+def add(num1, num2):
+  return num1 + num2
+
+def subtract(num1, num2):
+  return num1 - num2
+
+def multiply(num1, num2):
+  return num1 * num2
+
+def divide(num1, num2):
+  return num1 / num2
+
+def divide_with_remainder(num1, num2):
+  return str(round(num1 / num2)) + " R " + str(num1 % num2)
+
+def raise_to_power(num1, num2):
+  return num1 ** num2
